e43238c85a90c603af42b5cbf86a9e25851341d4
Matt Graham <m.j.graham@live.co.uk>
2020-12-14 09:58:57 +0000

    adding database model and migrations

--- px_sync/prescriptions/models.py
+++ px_sync/prescriptions/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+# SyncRequest.reference = a generated 6 character string
+class SyncRequest(models.Model):
+    reference = models.CharField(max_length=6)
+    created = models.DateTimeField(auto_now=True)
+
+# Prescription.frequency = frequency in days of prescription
+class Prescription(models.Model):
+    syncRequest = models.ForeignKey('SyncRequest', on_delete=models.CASCADE)
+    frequency = models.SmallIntegerField(default=0)
+
+# Quantity.prescribed - the amount present on a prescription
+# Quantity.inStock = the amount the patient has
+# Quantity.required = the computed amount required
+class Quantity(models.Model):
+    prescription = models.ForeignKey('Prescription', on_delete=models.CASCADE)
+    prescribed = models.SmallIntegerField(default=0)
+    inStock = models.SmallIntegerField(default=0)
+    required = models.SmallIntegerField(default=0)
+    drug = models.ForeignKey('Drug', on_delete=models.CASCADE)
+
+# A drug will be created irrespective of whether
+# the exact text exists in the database presently.
+# This is because small variations in user entry
+# will obfuscate LOWER(name) LIKE and other similar
+# non-fuzzy string matching.
+# Longer term, this is a candidate for replacing with
+# a link to the DM+D/CDR API, at which point this will
+# store the unique AMPP ID, an integer which is easier
+# to match on.
+class Drug(models.Model):
+    name = models.CharField(max_length=200)
--- px_sync/prescriptions/tests.py
+++ px_sync/prescriptions/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from prescriptions.models import SyncRequest, Prescription, Quantity, Drug
 
-# Create your tests here.
+class PrescriptionTests(TestCase):
+    def setUp(self):
+        pass
+
+    def test_syncrequest_instantiates(self):
+
+        syncRequest = SyncRequest()
+        self.assertIsInstance(syncRequest, SyncRequest)
+
+    def test_prescription_instantiates(self):
+
+        prescription = Prescription()
+        self.assertIsInstance(prescription, Prescription)
+        
+    def test_quantity_instantiates(self):
+
+        quantity = Quantity()
+        self.assertIsInstance(quantity, Quantity)
+
+    def test_drug_instantiates(self):
+
+        drug = Drug()
+        self.assertIsInstance(drug, Drug)

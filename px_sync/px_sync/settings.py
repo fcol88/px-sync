90aee8b6236b51730a76df8e57bb285c7bd9d1a4
Matt Graham <m.j.graham@live.co.uk>
2020-12-16 18:18:12 +0000

    adding note about css

--- px_sync/px_sync/settings.py
+++ px_sync/px_sync/settings.py
@@ -23,6 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'k-dh6k*_#p=v($khb7sgno$v%1k*4uw-!q2$je^2_dk8dc7(wz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# When running locally with debug switched off to test error pages,
+# remember to use the following command for CSS to work:
+# python manage.py runserver --insecure
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost']

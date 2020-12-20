a19d74c35ff4441583e56160d39459ef5b61fd83
Matt Graham <m.j.graham@live.co.uk>
2020-12-20 14:04:20 +0000

    trying to resolve mime type errors

--- px_sync/px_sync/settings/prod.py
+++ px_sync/px_sync/settings/prod.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -134,8 +134,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 SESSION_COOKIE_AGE = 3600 #one hour
 
